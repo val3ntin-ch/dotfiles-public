da010c995c150f809b80e827a326571bd9027911
V! <valentin.hetriuc@gmail.com>
2026-06-30 17:42:43 +0300

    fix(nvim): set COLORTERM=truecolor for lazygit inside nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,6 @@
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
+
+-- ensure terminals spawned inside nvim (lazygit, etc.) inherit true color support
+vim.env.COLORTERM = "truecolor"
