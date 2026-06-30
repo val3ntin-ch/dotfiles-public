48881f8f5d8e5840c58b0b930f652b9cc6cec0a6
V! <valentin.hetriuc@gmail.com>
2026-06-30 14:01:12 +0300

    fix(nvim): use tbl_deep_extend for neo-tree filtered_items merge

--- .config/nvim/lua/plugins/webdev.lua
+++ .config/nvim/lua/plugins/webdev.lua
@@ -33,13 +33,14 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
-      opts.filesystem = opts.filesystem or {}
-      opts.filesystem.filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_hidden = false,
-      }
+      opts.filesystem = vim.tbl_deep_extend("force", opts.filesystem or {}, {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      })
       return opts
     end,
   },
